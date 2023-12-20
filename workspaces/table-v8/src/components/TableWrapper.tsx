b88e4a8e29a401c75ff07ce3863e9d1f142acdb0
moon-ds <entwickler.js@gmail.com>
2023-12-20 15:26:17 +0500

    feat: the ability of keyboard scrolling has been added

--- workspaces/table-v8/src/components/TableWrapper.tsx
+++ workspaces/table-v8/src/components/TableWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, MutableRefObject, useEffect, WheelEvent } from "react"
+import React, { forwardRef, KeyboardEventHandler, MutableRefObject, useCallback, useEffect, useState, WheelEvent } from "react"
 import { mergeClassnames } from "@heathmont/moon-core-tw";
 import TableWrapperProps from "../private/types/TableWrapperProps";
 
@@ -6,6 +6,9 @@ const TableWrapper = forwardRef<HTMLDivElement, TableWrapperProps>(
   (
     { style, className, children, tableRef }
   ) => {
+    const kbDelta = 132;
+    const [isFocused, setIsFocused] = useState(false);
+
     const handleWheel = (e: globalThis.WheelEvent) => {
       const evt = e as unknown as WheelEvent<HTMLDivElement>;
       if ((evt.target as HTMLElement).closest('thead') !== null)
@@ -13,6 +16,22 @@ const TableWrapper = forwardRef<HTMLDivElement, TableWrapperProps>(
       evt.preventDefault();
       evt.currentTarget.scrollBy(0, evt.deltaY);
     }
+
+    const handleKbDown = useCallback((evt: React.KeyboardEvent<HTMLDivElement>) => {
+      if (isFocused) {
+        const kbDeltas = { x: 0, y: 0 };
+        if (evt.code === "ArrowUp" || evt.code === "ArrowDown" || evt.code === "ArrowLeft" || evt.code === "ArrowRight") {
+          evt.preventDefault();
+          switch (evt.code) {
+            case "ArrowUp": kbDeltas.y = -kbDelta; break;
+            case "ArrowDown": kbDeltas.y = kbDelta; break;
+            case "ArrowLeft": kbDeltas.x = -kbDelta; break;
+            case "ArrowRight": kbDeltas.x = kbDelta; break;
+          }
+          evt.currentTarget.scrollBy(kbDeltas.x, kbDeltas.y);
+        }
+      }
+    }, [isFocused]);
 
     useEffect(() => {
       const element = (tableRef as MutableRefObject<HTMLDivElement>)?.current;
@@ -22,10 +41,15 @@ const TableWrapper = forwardRef<HTMLDivElement, TableWrapperProps>(
     return (
       <div
         ref={tableRef}
+        tabIndex={0}
         style={style ?? {}}
         className={mergeClassnames(
-          className
+          "focus:outline-none",
+          className,
         )}
+        onFocus={() => { setIsFocused(true) }}
+        onBlur={() => { setIsFocused(false) }}
+        onKeyDown={(e) => { handleKbDown(e) }}
       >
         {children}
       </div>
